0367c9e78f74b9947bac9fa16332a6aceba5d7ab
Beny Finkelstein <benyfink@MacBook-Air-de-Beny.local>
2025-12-12 16:27:32 -0300

    descrição da mudança

--- api/apisave.php
+++ api/apisave.php
@@ -278,9 +278,9 @@ try {
 
     if ($table === 'reservations') {
       try {
-        agendarEnvioEmailsReserva($pdo, $id);
+        enviarEmailReservaSolicitada($pdo, $id);
       } catch (Throwable $mailError) {
-        error_log('Erro ao agendar e-mails da reserva: ' . $mailError->getMessage());
+        error_log('Erro ao enviar e-mail da reserva: ' . $mailError->getMessage());
       }
     }
 
@@ -319,17 +319,6 @@ function sincronizarVisitantesReserva(PDO $pdo, $reservationId, array $visitors)
       $stmtIns->execute([$reservationId, $visitorId]);
     }
   }
-}
-
-function agendarEnvioEmailsReserva(PDO $pdo, int $reservationId): void {
-  $pdoRef = $pdo;
-  register_shutdown_function(static function () use ($pdoRef, $reservationId) {
-    try {
-      enviarEmailReservaSolicitada($pdoRef, $reservationId);
-    } catch (Throwable $err) {
-      error_log("[MAIL] Falha ao enviar e-mail para cliente da reserva #{$reservationId}: " . $err->getMessage());
-    }
-  });
 }
 
 function enviarEmailReservaSolicitada(PDO $pdo, int $reservationId): void {
