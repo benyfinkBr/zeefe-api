5bd4d4367bab8dec3d80ee740f342ba13b421f51
Beny Finkelstein <benyfink@MacBook-Air-de-Beny.local>
2026-02-13 14:10:05 -0300

    Inclusão Pesquisa

--- api/survey_builder.php
+++ api/survey_builder.php
@@ -2,6 +2,9 @@
 require_once 'apiconfig.php';
 
 header('Content-Type: application/json');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
 
 if (empty($_SESSION['admin_id'])) {
   http_response_code(401);
